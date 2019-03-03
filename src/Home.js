e5ca227c6184aa351c8df6a01f3c2f39931d2c9a
tui1117 <tui1117@g.rit.edu>
2019-03-03 09:00:36 -0500

    Background image added to homepage buttons updated.

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import BackgroundImage from './hompageback.jpg';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import pink from '@material-ui/core/colors/pink';
 
 
-class Home extends Component {
-    render() {
+const styles = theme => ({
+  button: {
+    margin: '100px',
+  },
+  cssRoot: {
+    color: theme.palette.getContrastText(pink[200]),
+    margin: '10px',
+    fontFamily: 'Montserrat',
+    height: '80px',
+    backgroundColor: '#e1b1af',
+    '&:hover': {
+      backgroundColor: pink[700],
+    },
+  },
+  input: {
+    display: 'none',
+  },
+});
+
+
+function Home(props) {
+  const { classes } = props;
         return (
-                <body>
-                    <div className="App">
+                    <div className="App" style={{backgroundImage: 'url(' + BackgroundImage + ')', width: '100%', height: '100%', backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat',}}>
                         <header className="App-header">
-                            <p>Firm Fatale</p>
-                            <Link to="/find-a-femme"><button className="btn findafemme">Find-a-Femme</button></Link>
-                            <Link to="/card-share"><button className="btn findafemme">Card Share</button></Link>
+                            <h1>Firm Fatale</h1>
+                            <p className="subtitle">Helping women in business build their ideal femme-pire!</p>
+
+                            <Link to="/find-a-femme"><Button variant="contained" color="primary" size="large" variant="contained"className={classes.button, classes.cssRoot}>Find-a-Femme</Button></Link>
+                            <Link to="/card-share"><Button variant="contained" color="primary" size="large" variant="contained"className={classes.button, classes.cssRoot}>Card Share</Button></Link>
                         </header>
 
                     </div>
-                </body>
         );
-    }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
